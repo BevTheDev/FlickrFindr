8e6a24e8c903fd1721887aead62aeba2fe67bbe6
bmassengill <beverly.massengill@gmail.com>
2020-01-17 17:38:15 -0600

    Initial project setup

---
+++ FlickrFindr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrFindr
+//
+//  Created by Beverly Massengill on 1/17/20.
+//  Copyright © 2020 BevTheDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+source 'https://cdn.cocoapods.org/'
+
+platform :ios, '12.0'
+
+inhibit_all_warnings!
+use_frameworks!
+
+workspace 'FlickrFindr'
+project 'FlickrFindr'
+
+target 'FlickrFindr' do
+
+  # Testing
+  target 'FlickrFindrTests' do
+    inherit! :search_paths
+    pod 'OHHTTPStubs/Swift'
+  end
+end
