b3c1171d9486f1a20fd0dfb942860ffe8b47cf08
bmassengill <beverly.massengill@gmail.com>
2020-01-22 23:41:57 -0600

    Rotation support for fullscreen images

--- FlickrFindr/View Controllers/Image Viewing/FullScreenImageViewController.swift
+++ FlickrFindr/View Controllers/Image Viewing/FullScreenImageViewController.swift
@@ -11,7 +11,13 @@ import UIKit
 class FullScreenImageViewController: UIViewController, SpinnerShowable {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleBackdrop: UIView!
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageTrailingConstraint: NSLayoutConstraint!
     
     let photo: Photo
     var spinnerView = SpinnerView()
@@ -33,17 +39,66 @@ class FullScreenImageViewController: UIViewController, SpinnerShowable {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
+        
+        setUpUI()
+        addTitleShadow()
+        
         titleLabel.text = photo.title
+        titleBackdrop.isHidden = photo.title.isEmpty
+        
+        loadImage(forPhoto: photo)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapImage))
+        imageView.addGestureRecognizer(tapGesture)
+    }
+    
+    func addTitleShadow() {
+        
+        titleBackdrop.layer.masksToBounds = false
+        titleBackdrop.layer.shadowOffset = CGSize(width: 0, height: -5)
+        titleBackdrop.layer.shadowRadius = 3
+        titleBackdrop.layer.shadowOpacity = 1
+    }
+    
+    func loadImage(forPhoto photo: Photo) {
         
         let imageUrl = photo.imageUrl(forSize: .fullscreen)
         
         showActivitySpinner(fromView: imageView)
         
-        print("Fullscreen: \(imageUrl)")
+        print("Loading fullscreen image: \(imageUrl)")
         imageView.loadWebImage(fromUrl: imageUrl) {
             self.hideActivitySpinner()
         }
+    }
+    
+    // MARK: - Rotation Handling
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        setUpUI()
+    }
+    
+    func setUpUI() {
+        
+        if UIDevice.current.orientation.isPortrait {
+            
+            imageTopConstraint.constant = 90
+            imageBottomConstraint.constant = 90
+            imageLeadingConstraint.constant = 0
+            imageTrailingConstraint.constant = 0
+        }
+        else {
+            
+            imageTopConstraint.constant = 0
+            imageBottomConstraint.constant = 0
+            imageLeadingConstraint.constant = 55
+            imageTrailingConstraint.constant = 55
+        }
+        
+        view.layoutIfNeeded()
     }
     
     // MARK: - IBActions
@@ -51,5 +106,17 @@ class FullScreenImageViewController: UIViewController, SpinnerShowable {
     @IBAction func closeTapped() {
         
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func didTapImage() {
+        
+        let newTitleAlpha: CGFloat =  self.titleLabel.alpha == 0 ? 1 : 0
+        let newBackdropAlpha: CGFloat =  self.titleBackdrop.alpha == 0 ? 0.6 : 0
+        
+        UIView.animate(withDuration: 0.5) {
+            
+            self.titleLabel.alpha = newTitleAlpha
+            self.titleBackdrop.alpha = newBackdropAlpha
+        }
     }
 }
